e61907cb48c366f07615c856a2f29caa9dd71b17
Hardik Srivastava <hardiksrivastav.vns@gmail.com>
2023-07-16 05:51:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ function App() {
             <li>
               <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </li>
+            <li>
+              <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">Google</a>
+            </li>
           </ul>
         </nav>
       </header>
